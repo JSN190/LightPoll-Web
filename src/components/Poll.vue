be4c96f147f1189e573f19749122bb5501709380
John Su <30964725+JSN190@users.noreply.github.com>
2018-09-11 03:34:16 +0100

    Create Poll template

---
+++ src/components/Poll.vue
@@ -0,0 +1,33 @@
+<template>
+    <main class="container">
+        {{ this.$route.params.id }}
+        <div class="columns">
+            <div class="column  is-two-thirds">
+                First column
+            </div>
+            <div class="column is-one-third">
+                Second column
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: "Poll",
+    data: {
+        name: "",
+        description:  "",
+        options: []
+    },
+    mounted: function () {
+        //
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
