8103f794d617dbe06f51ee8cc83e6fb717d7cb48
John Su <30964725+JSN190@users.noreply.github.com>
2018-09-11 13:23:48 +0100

    Create component file for easier injection of the top loading bar

---
+++ src/components/TopLoadingBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="top-loading-bar-container">
+            <div class="flat-loading-bar"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TopLoadingBar"
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/common/flat-loading-bar/style.scss";
+</style>
+
