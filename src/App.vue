0c385a09ac692509498f4052a51093f533f888f5
John Su <30964725+JSN190@users.noreply.github.com>
2018-09-12 02:10:03 +0100

    Reorganise CSS/SCSS imports

--- src/App.vue
+++ src/App.vue
@@ -22,11 +22,17 @@ export default {
 <style lang="scss">
 @import url("https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css");
 @import url("https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css");
-@import url("https://fonts.googleapis.com/css?family=Roboto:400,400i,500,500i,700,700i");
-@import "./styles/common.scss";
+@import url('https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i');
+@import url("https://use.fontawesome.com/releases/v5.3.1/css/all.css");
+@import "./common/basics/style.scss";
 
-* {
-  box-sizing: border-box;
-  font-family: 'Roboto', sans-serif;
+body {
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+}
+
+.fas, .fab, .far, .fal {
+    // Reset for font-awesome
+    font-weight: 900 !important;
 }
 </style>
