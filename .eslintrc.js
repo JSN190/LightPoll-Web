4b8a26cd4b91959b5adb6afa84b1a3c6246de478
John Su <30964725+JSN190@users.noreply.github.com>
2018-09-12 09:31:07 +0100

    Dot files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es6": true
+        "es6": true,
+        "node": true
     },
     "extends": [
         "eslint:recommended",
         "plugin:vue/essential"
     ],
     "parserOptions": {
-        "ecmaVersion": 2017,
+        "ecmaVersion": 2018,
         "sourceType": "module"
     },
     "rules": {
